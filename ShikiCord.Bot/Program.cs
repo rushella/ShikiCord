6e53a0109041503fc68d828468df88d8368d1fca
rushella <rushella.ln@outlook.com>
2022-03-23 01:57:30 +0300

    db initial migration added

--- ShikiCord.Bot/Program.cs
+++ ShikiCord.Bot/Program.cs
@@ -1,22 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using ShikiCord.Db;
 
 var environment = Environment.GetEnvironmentVariable("SHIKICORD_ENVIRONMENT")!;
 
 var configuration = new ConfigurationBuilder()
-    .AddJsonFile($"appsettings.json", false, true)
-    .AddJsonFile($"appsettings.{environment}.json", true, true)
+    .AddJsonFile($"botsettings.json", false, true)
+    .AddJsonFile($"botsettings.{environment}.json", true, true)
     .AddEnvironmentVariables()
     .Build();
 
 var serviceProvider = new ServiceCollection()
     .AddSingleton(configuration)
-    .AddSingleton<ShikiCordContext>()
-    .AddSingleton<DbContextOptions<ShikiCordContext>>()
-    .AddDbContext<ShikiCordContext>(
-        (optionsBuilder) => optionsBuilder.UseNpgsql(configuration.GetConnectionString("ShikiCordDb")!))
+
     .BuildServiceProvider();
-    
-    
---
+++ ShikiCord.Db/Migrations/20220322225507_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShikiCord.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    DiscordId = table.Column<decimal>(type: "numeric(20,0)", nullable: false),
+                    ShikimoriId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.DiscordId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
